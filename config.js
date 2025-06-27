133ef144b2d93609d2231ef4de2e6dffbbf1c968
malishanethsar <malishanethsara647@gmail.com>
2025-06-27 21:09:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "+94775309776",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "+94788008391",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "𝗠𝐚𝐥𝐢𝐲𝐚 MD",
 // add bot owner name
