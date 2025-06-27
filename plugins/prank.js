c9307f06e1209535d432044f9b41c8bc71971887
simi00png <simimd7773@gmail.com>
2025-06-27 09:10:07 +0100

    Update prank.js

--- plugins/prank.js
+++ plugins/prank.js
@@ -35,7 +35,7 @@ async (conn, mek, m, { from, quoted, reply }) => {
             '⚠️ _This is a simulated hacking activity for entertainment purposes._',
             '⚠️ _Remember: Ethical hacking ensures safety._',
             '',
-            '> *𝐁.𝐌.𝐁-𝐗𝐌𝐃: HACKING SIMULATION COMPLETE* ☣'
+            '> *𝗠𝐚𝐥𝐢𝐲𝐚 MD HACKING SIMULATION COMPLETE* ☣'
         ];
 
         for (const step of steps) {
